abbd7a8f4a953b30ca9e815c271f36a428fe671a
Rillcod Academy <info@rillcod.com>
2026-03-21 22:42:09 +0100

    Chore: clean up all temporary power shell scripts

--- tmp-read-nav.ps1
+++
@@ -1,3 +0,0 @@
-$result = & git -C "c:\Users\USER\Downloads\rillcod-academy-main\rillcod-academy-main" show "c9ebfea:src/components/layout/DashboardNavigation.tsx" 2>&1
-$result | Out-File -FilePath "c:\Users\USER\Downloads\rillcod-academy-main\rillcod-academy-main\tmp-nav-old.txt" -Encoding UTF8
-Write-Host "Done. Lines: $($result.Count)"
